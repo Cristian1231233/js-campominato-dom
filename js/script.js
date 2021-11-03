137cd5cae73d9335630c46ad4e5602b853fc35c0
Cristian1231233 <campagnacristian22@gmail.com>
2021-11-03 23:20:45 +0100

    ulteriore commit

--- js/script.js
+++ js/script.js
@@ -1,106 +1,148 @@
 
 
 document.querySelector('.play').addEventListener('click', function(){
-   play();
-});
-
-function play(){
-    const level = document.getElementById('selezione').value;
-    console.log(level);
-    
-    let numeroCelle;
-    if(level === 'easy'){
-        numeroCelle = 100;
-        console.log(numeroCelle);
-    }else if(level === 'crasy'){
-        numeroCelle = 81;
-        console.log(numeroCelle);
-    }else if(level === 'hard'){
-        numeroCelle = 49;
-        console.log(numeroCelle);
-    }
-    
-    const celleForRow = Math.sqrt(numeroCelle);
-    console.log(celleForRow);
-
-
-    document.querySelector('main').innerHTML = '';
-    generateGrid();
-
-    function generateGrid(){
-        const grid = document.createElement('div');
-
-        grid.className = 'countain-wrap';
-
-        document.querySelector('main').append(grid);
-
-        for( let i = 1; i <= numeroCelle; i++){
-        //    creo ogni cella
-           const cella = document.createElement('div');
-           cella.className = 'square';
-           cella.innerHTML = `<span>${i}</span>`;
-           const cellaSize = `calc(100% / ${celleForRow})`;
-           cella.style.width = cellaSize;
-           cella.style.height = cellaSize;
-           
-           grid.append(cella);
-
-           cella.addEventListener('click', function(){
-               this.classList.add('clicked');
-               console.log('cliccata');
-           });
-        }
-
-
-    }
-
-
-    
-
-}
-
-
-
-
-// const testo = document.querySelector('.testo');
-
-// let selezione = document.getElementById('selezione');
-// console.log(selezione);
-
-
-// function getselectedValue(){
-//     let selectedValue = document.getElementById('selezione');
-//     console.log(selectedValue);
-// }
-// getselectedValue();
-
-
-// play.addEventListener('click', function(){
-  
-
+    play();
+ });
  
-//     let container = document.querySelector('.countain-wrap');
-//     console.log(container);
-    
-// // creo un ciclo for in cui inserisco gli square
-// for( let i = 1; i < 101; i++){
-    
-//     const sq = document.createElement('div');
-//     sq.className = 'square';
-//     sq.innerHTML = `<span> ${numerRandom(1, 100)}</span>`;
-//     container.append(sq);
-//     sq.addEventListener('click', function(){
-//         this.classList.add('clicked');
-//     });
-  
-// }
+ function play(){
+     const level = document.getElementById('selezione').value;
+     console.log(level);
+     
+     let numeroCelle;
+     if(level === 'easy'){
+         numeroCelle = 100;
+         console.log(numeroCelle);
+     }else if(level === 'crasy'){
+         numeroCelle = 81;
+         console.log(numeroCelle);
+     }else if(level === 'hard'){
+         numeroCelle = 49;
+         console.log(numeroCelle);
+     }
 
-//     testo.classList.add('none');
-//     containerHidd.classList.remove('none');
-    
-// });
+     
+     
+     const celleForRow = Math.sqrt(numeroCelle);
+     console.log(celleForRow);
+          
+     
+     
 
+     document.querySelector('main').innerHTML = '';
+     generateGrid();
+ 
+     function generateGrid(){
+         const grid = document.createElement('div');
+ 
+         grid.className = 'countain-wrap';
+ 
+         document.querySelector('main').append(grid);
+ 
+         for( let i = 1; i <= numeroCelle; i++){
+         //    creo ogni cella
+            const cella = document.createElement('div');
+            cella.className = 'square';
+            cella.innerHTML = `<span>${i}</span>`;
+            const cellaSize = `calc(100% / ${celleForRow})`;
+            cella.style.width = cellaSize;
+            cella.style.height = cellaSize;
+            
+            grid.append(cella);
+ 
+            cella.addEventListener('click', eventClick);
+                
+            
+         }
+          
+         const BOMBS_NUMBER = 16;
+         const bombs = generateBombs();
+
+           function eventClick(event){
+               console.log(event.target.innerText);
+               this.classList.add('clicked');
+               
+           }
+
+        
+         
+ 
+         function generateBombs(){
+             const bombs = [];
+              console.log('BOMBS_NUMBER', BOMBS_NUMBER);
+
+             while(bombs.length < BOMBS_NUMBER){
+                 const bomb = randomBomb(1, numeroCelle);
+               if(bombs.includes(bomb)){
+                bombs.push(bomb);
+               }
+                 
+             }
+             return bombs;
+         }
+     }
+ 
+
+
+        
+     
+ 
+ }
+ 
+ function randomBomb(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+ 
+ 
+ // const testo = document.querySelector('.testo');
+ 
+ // let selezione = document.getElementById('selezione');
+ // console.log(selezione);
+ 
+ 
+ // function getselectedValue(){
+ //     let selectedValue = document.getElementById('selezione');
+ //     console.log(selectedValue);
+ // }
+ // getselectedValue();
+ 
+ 
+ // play.addEventListener('click', function(){
+   
+ 
   
+ //     let container = document.querySelector('.countain-wrap');
+ //     console.log(container);
+     
+ // // creo un ciclo for in cui inserisco gli square
+ // for( let i = 1; i < 101; i++){
+     
+ //     const sq = document.createElement('div');
+ //     sq.className = 'square';
+ //     sq.innerHTML = `<span> ${numerRandom(1, 100)}</span>`;
+ //     container.append(sq);
+ //     sq.addEventListener('click', function(){
+ //         this.classList.add('clicked');
+ //     });
+   
+ // }
+ 
+ //     testo.classList.add('none');
+ //     containerHidd.classList.remove('none');
+     
+ // });
+ 
+   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // function numerRandom(min, max){
+ //     return Math.floor(Math.random() * (max - min + 1) + min)
+ 
 
 
 
